b05f0793655fa6a2f9e2b4d43cdf4fe8200afb74
John Rey Castro Sobrepeña <148199376+jhnrysbrpn@users.noreply.github.com>
2024-06-17 19:56:07 +0800

    Zero (#9)
    * PsychConnect
    
    * link fixed
    
    * fireworks
    
    * New Year Countdown
    
    * Countdown added
    
    * adjusting styles
    
    * Christmas Countdown
    
    * Adjusting design
    
    * Update newyear.html
    
    * slight adjustment has been made
    
    * added some text and styles
    
    * Update newyear.js
    
    * fixed
    
    * BMV
    
    * added files
    
    * files added

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "../Components/Nav";
+import AppSpline from "../src/AppSpline";
+function App() {
+
+  return (
+    <>
+     <Nav/>
+     <AppSpline/>
+    </>
+  )
+}
+
+export default App
