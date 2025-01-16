71a3dbc1f7a9705505734d4758992914e305e1a2
John Rey Sobrepeña <sobrepenajohnrey023@gmail.com>
2025-01-16 12:06:20 +0800

    update

--- itsjustme/src/App.jsx
+++ itsjustme/src/App.jsx
@@ -6,6 +6,20 @@ function App() {
   return (
     <>
        <div>
+        <p> It's Just Me </p>
+          <div className=''>
+            <video src=""></video>
+          </div>
+       </div>
+
+       <div>
+        <p>Wanna Know more about me? </p>
+        <div className=''>
+          <a href=''>Introduction</a>   
+          <a href=''>Hobby</a>   
+          <a href=''>Introduction</a>   
+          <a href=''>Introduction</a>   
+        </div>
        </div>
     </>
   )
