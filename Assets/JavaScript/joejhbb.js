11b02f894907259c31c2acfa9468813a079d33e6
jhnrysbrpn <sobrepenajohnrey023@gmail.com>
2025-05-29 23:55:20 +0800

    some changes

---
+++ Assets/JavaScript/joejhbb.js
@@ -0,0 +1,10 @@
+var name = " Johannah S. Agapito " ;
+var age = 20 ;
+var birthday = " 2005-03-03 " ;
+var place = " San Jose Montalban, Rizal " ;
+var hobbies = [ " Reading " , " Singing " , " Dancing " , " Playing the guitar/Ukelele " ] ;
+var education = " Bachelor of Science in Information Technology " ;
+var skills =  " HTML , CSS , JavaScript , Bootstrap , C , C++ , Python " ;
+
+var aboutMe = "I'm a Second year student at Polytechnic University of the Philippines taking up Bachelor of Science in Information Technology. I am a hardworking and passionate person who loves to learn new things. As an Aspiring Web Developer with strong dedication in Learning and Upscaling skills, I am looking for opportunities to grow and develop my skills in the field of technology." ;
+
